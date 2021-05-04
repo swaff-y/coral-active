cd82a84cedf0b9006d9bcd7a1ac5cc174192e71e
swaff-y <kyle@swaff.id.au>
2021-05-05 07:18:17 +1000

    Added swap between ordered list and unordered list

--- js/main.js
+++ js/main.js
@@ -4,16 +4,31 @@ console.log("https://github.com/swaff-y/coral-active/blob/main/js/main.js");
 const array = ["one","two",["three",["four"],"five"],"six"];
 const arrayTwo = ["one", {name: "two", nodes: [{name: "2.1", nodes: ["2.1a"]}, "two point two"]}, "three"];
 let str = "";
+let strD = "";
 
 const isObject = (obj) => {
   return obj === Object(obj);
 }
 
-const recursive = (array) => {
+const recursive = (array,type = false) => {
+  let orderedListFront = "";
+  let orderedListBack = "";
+  let orderedListItemFront = "";
+  type === true ? orderedListFront = "<ol class='ol'>" : orderedListFront = "<ul>"
+  type === true ? orderedListBack = "</ol>" : orderedListBack = "</ul>"
+  // const orderedListItemFront = "<li>"
+  type === true ? orderedListItemFront = "<li class='li'>)" : orderedListItemFront = "<li>"
+
+
   const testArrayIn = Array.isArray(array);
   // check if we dealing with an array or object
   if(testArrayIn === true){
-    str = str + "<ol>"; //Print out first <UL>
+    //Print out first <UL>
+    type === true
+    ?
+    str = str + orderedListFront
+    :
+    strD = strD + orderedListFront;
     //Go through all elements in the array
     for( let i = 0; i < array.length; i++ ){
         //Test if the element is an Object or not
@@ -22,18 +37,30 @@ const recursive = (array) => {
           const arr = Object.values(array[i]);
           //always choosing the first "Name" - Kind of going back a step
           //print out the list item
-          str = str + `<li>) ${ arr[0]}</li>`;
+          type === true
+          ?
+          str = str + `${orderedListItemFront} ${ arr[0]}</li>`
+          :
+          strD = strD + `${orderedListItemFront} ${ arr[0]}</li>`;
         }else{
           // if not an object print the element
           //print out the "node" part of the object
-          str = str + `<li>) ${ array[i]}</li>`;
+          type === true
+          ?
+          str = str + `${orderedListItemFront} ${ array[i]}</li>`
+          :
+          strD = strD + `${orderedListItemFront} ${ array[i]}</li>`;
         }
         //After printing out -> recursivly call the function again to
         //check array items again for each element
-        recursive(array[i]);
+        recursive(array[i],type);
     }
     //the UL gets added recursivly after the depth first search is complete
-    str = str + "</ol>";
+    type === true
+    ?
+    str = str + orderedListBack
+    :
+    strD = strD + orderedListBack;
   }else{
     //if we are not dealing with an array but rather an object
     //create an array from the values and loop through to
@@ -41,12 +68,19 @@ const recursive = (array) => {
     const arr = Object.values(array);
     for( let i = 0; i < arr.length; i++ ){
       if(isObject(arr[i]) === true){
-        recursive(arr[i]);
+        recursive(arr[i],type);
       }
     }
   }
-  return str
+
+  if(type === true){
+    return str
+  }else{
+    return strD
+  }
+  // return str
 }
+
 
 // const recursive = (array) => {
 //   const test = Array.isArray(array);
